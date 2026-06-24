aca44106f953cc4b84c1e605d918a07d901a3fa7
Rony Maciel <128644612+RonyMac@users.noreply.github.com>
2026-06-24 15:06:11 -0300

    Add data loading script for raw_clientes table
    This script extracts data from a CSV file and loads it into a SQLite database.

---
+++ src_ingestao/carregar_dados.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Extract
+df = pd.read_csv('../data/clientes_brutos.csv')
+
+# Load (Injeta o dado bruto na tabela 'raw_clientes')
+engine = create_engine('sqlite:///../empresa.db')
+df.to_sql('raw_clientes', con=engine, if_exists='replace', index=False)
+
+print("Etapa de Extract e Load concluída!")
