8d89771e6bf9180dda2a0ebf5d831e23c222a0c5
Rony Maciel <128644612+RonyMac@users.noreply.github.com>
2026-07-01 00:52:33 -0300

    Fix database path in carregar_dados.py

--- src_ingestao/carregar_dados.py
+++ src_ingestao/carregar_dados.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 df = pd.read_csv('data/clientes_brutos.csv')
 
 # Load (Injeta o dado bruto na tabela 'raw_clientes')
-engine = create_engine('sqlite:///../empresa.db')
+engine = create_engine('sqlite:///empresa.db')
 df.to_sql('raw_clientes', con=engine, if_exists='replace', index=False)
 
 print("Etapa de Extract e Load concluída!")
