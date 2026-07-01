6ee28314e7fbc336bf10e0d0e04d7d8fee2f80a9
Rony Maciel <128644612+RonyMac@users.noreply.github.com>
2026-07-01 00:29:23 -0300

    Fix CSV file path in carregar_dados.py
    Updated the path for reading the CSV file to be relative to the current directory.

--- src_ingestao/carregar_dados.py
+++ src_ingestao/carregar_dados.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # Extract
-df = pd.read_csv('../data/clientes_brutos.csv')
+df = pd.read_csv('data/clientes_brutos.csv')
 
 # Load (Injeta o dado bruto na tabela 'raw_clientes')
 engine = create_engine('sqlite:///../empresa.db')
